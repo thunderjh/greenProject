ebf9ae4c9279db9d2f28dfd4aad02e3fd711a95e
Hyde <109658378+skys1924@users.noreply.github.com>
2022-08-13 22:53:41 +0900

    Delete greenProject directory

--- greenProject/src/main/java/com/spring/admin/login/dao/AdminLoginDao.java
+++
@@ -1,7 +0,0 @@
-package com.spring.admin.login.dao;
-
-import com.spring.admin.login.vo.AdminLoginVO;
-
-public interface AdminLoginDao {
-	public AdminLoginVO loginProcess(AdminLoginVO login);
-}
--- greenProject/src/main/java/com/spring/admin/login/service/AdminLoginService.java
+++
@@ -1,7 +0,0 @@
-package com.spring.admin.login.service;
-
-import com.spring.admin.login.vo.AdminLoginVO;
-
-public interface AdminLoginService {
-	public AdminLoginVO loginProcess(AdminLoginVO login);
-}
